314b332864c27a58d0ce5ebb6104eabe6b314947
alansif <alansif@sina.com>
2017-08-15 16:32:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="app">
+        <el-menu class="navbar" mode="horizontal">
+            <el-dropdown class="avatar-container" trigger="click">
+                <div class="avatar-wrapper">
+                    <i class="el-icon-caret-bottom"></i>
+                </div>
+                <el-dropdown-menu class="user-dropdown" slot="dropdown">
+                    <router-link class='inlineBlock' to="/">
+                        <el-dropdown-item>
+                            Home
+                        </el-dropdown-item>
+                    </router-link>
+                    <el-dropdown-item divided>
+                        <span @click="logout" style="display:block;">LogOut</span>
+                    </el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </el-menu>
+        <router-view></router-view>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            logout() {
+
+            }
+        }
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .navbar {
+        height: 50px;
+        line-height: 50px;
+        border-radius: 0px !important;
+        .hamburger-container {
+            line-height: 58px;
+            height: 50px;
+            float: left;
+            padding: 0 10px;
+        }
+        .errLog-container {
+            display: inline-block;
+            position: absolute;
+            right: 150px;
+        }
+        .screenfull {
+            position: absolute;
+            right: 90px;
+            top: 16px;
+            color: red;
+        }
+        .avatar-container {
+            height: 50px;
+            display: inline-block;
+            position: absolute;
+            right: 35px;
+            .avatar-wrapper {
+                cursor: pointer;
+                margin-top: 5px;
+                position: relative;
+                .user-avatar {
+                    width: 40px;
+                    height: 40px;
+                    border-radius: 10px;
+                }
+                .el-icon-caret-bottom {
+                    position: absolute;
+                    right: -20px;
+                    top: 25px;
+                    font-size: 12px;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import layout from './components/layout.vue'
+
+export default [
+    { path:'/', component:layout }
+]
